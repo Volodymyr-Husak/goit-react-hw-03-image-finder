a4fd1c028f6a75e4a591aef0ed80926be4ff458f
Volodymyr Husak <volodyaog2907@gmail.com>
2022-10-30 01:51:02 +0300

    corrects component

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,10 +1,36 @@
 import { Component } from 'react';
 import { ImageGalleryItem } from './ImageGalleryItem';
 export class ImageGallery extends Component {
+  // KEY = '29882224-53e6cb6eb5c61ad27904c20c4';
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevProps.imageName !== this.props.imageName) {
+  //     // alert('ALLLERT');
+  //     // console.log('ALLLERT')
+  //     fetch(
+  //       `https://pixabay.com/api/?q=${this.props.imageName}&page=1&key=${this.KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  //     )
+  //       .then(res => res.json())
+  //       .then(obj => this.props.arrImageWithFetch(obj.hits));
+  //     // .then(console.log);
+  //   }
+  // }
+  // console.log()
   render() {
     return (
       <ul className="gallery">
-        <ImageGalleryItem imageName={this.props.imageName }/>
+        {/* {this.props.arrImage.map(({id}) => id)} */}
+        {this.props.arrImage.map(({ id, webformatURL, largeImageURL, tags}) => {
+          return (
+            <ImageGalleryItem
+              key={id}
+              webformatURL={webformatURL}
+              largeImageURL={largeImageURL}
+              tags={tags}
+            />
+          );
+        })}
+        {/* {this.props.arrImage.map(({ id }) => { return <p>{id}</p> })} */}
+        {/* <ImageGalleryItem /> */}
       </ul>
     );
   }
--- src/components/ImageGallery/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem.jsx
@@ -1,17 +1,18 @@
 import { Component } from 'react';
 
 export class ImageGalleryItem extends Component {
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.imageName !== this.props.imageName) {
-        alert('ALLLERT');
-        // console.log('ALLLERT')
-    }
-  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevProps.imageName !== this.props.imageName) {
+  //       alert('ALLLERT');
+  //       // console.log('ALLLERT')
+  //   }
+  // }
+  // console.log(this.props.id)
   render() {
     return (
       <li className="gallery-item">
-        <p>ITEM</p>
-        <img src="" alt="" />
+        {/* <p>ITEM</p> */}
+        <img src={this.props.webformatURL} alt={this.props.tags} />
       </li>
     );
   }
